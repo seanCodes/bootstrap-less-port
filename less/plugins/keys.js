102a419740ae4989678a5ddd0a7f3a7169633ce5
Calvin Juárez <calvin.juarez@gmail.com>
2018-07-16 22:47:44 -0600

    each – replace all #each-* loop with each or #for-*
    There are 4 loops in mixins/_grid-framework.less that loop 'til a static number.  I'm not aware of any answer for that at the moment, but a plugin could be easily written that could just leverage `each()`.

---
+++ less/plugins/keys.js
@@ -0,0 +1,12 @@
+functions.add('keys', function ({ ruleset: { rules } }) {
+	const keys = []
+
+	rules.forEach(({ name: key }) => {
+		if (typeof key !== 'string' && key.length === 1 && (key[0] instanceof tree.Keyword)) // logic borrowed from https://github.com/less/less.js/blob/master/lib/less/tree/declaration.js#L46-L49
+			key = key[0].value // this may be a touch brittle
+
+		keys.push(new tree.Anonymous(key))
+	})
+
+	return new tree.Value(keys)
+})
